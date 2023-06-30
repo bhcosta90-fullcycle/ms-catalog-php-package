9485b08d4d8b33d05f9a834eb64dc039cbe1a2bf
Bruno Costa <bruno.costa@dafiti.com.br>
2023-06-30 08:27:35 -0300

    finalizado

--- src/Domain/Entity/Video.php
+++ src/Domain/Entity/Video.php
@@ -20,11 +20,53 @@ class Video
         protected int $duration,
         protected bool $opened,
         protected Rating $rating,
+        protected array $categories = [],
+        protected array $genres = [],
+        protected array $castMembers = [],
         protected bool $publish = false,
         protected ?Uuid $id = null,
         protected ?DateTime $createdAt = null,
     ) {
         $this->id = $this->id ?: Uuid::make();
         $this->createdAt = $this->createdAt ?: new DateTime();
+    }
+
+    public function addCategory(string $category)
+    {
+        array_push($this->categories, $category);
+    }
+
+    public function removeCategory(string $category)
+    {
+        if (array_search($category, $this->categories) !== false) {
+            unset($this->categories[array_search($category, $this->categories)]);
+            $this->categories = array_values($this->categories);
+        }
+    }
+
+    public function addGenre(string $genre)
+    {
+        array_push($this->genres, $genre);
+    }
+
+    public function removeGenre(string $genre)
+    {
+        if (array_search($genre, $this->genres) !== false) {
+            unset($this->genres[array_search($genre, $this->genres)]);
+            $this->genres = array_values($this->genres);
+        }
+    }
+
+    public function addCastMember(string $castMember)
+    {
+        array_push($this->castMembers, $castMember);
+    }
+
+    public function removeCastMember(string $castMember)
+    {
+        if (array_search($castMember, $this->castMembers) !== false) {
+            unset($this->castMembers[array_search($castMember, $this->castMembers)]);
+            $this->castMembers = array_values($this->castMembers);
+        }
     }
 }
--- tests/Unit/Domain/Entity/VideoTest.php
+++ tests/Unit/Domain/Entity/VideoTest.php
@@ -14,6 +14,8 @@ test('attributes', function () {
         duration: 50,
         opened: true,
         rating: Rating::L,
+        publish: true,
+        createdAt: $dateTime = new DateTime(),
     );
 
     expect($domain->id())->toBe($id);
@@ -23,5 +25,129 @@ test('attributes', function () {
     expect($domain->duration)->toBe(50);
     expect($domain->opened)->toBeTrue();
     expect($domain->rating->value)->toBe('L');
+    expect($domain->publish)->toBeTrue();
+    expect($domain->createdAt())->toBe($dateTime->format('Y-m-d H:i:s'));
+});
+
+test('create', function () {
+    $domain = new Video(
+        title: 'title',
+        description: 'description',
+        yearLaunched: 2010,
+        duration: 50,
+        opened: true,
+        rating: Rating::L,
+    );
+
+    expect($domain->id())->not->toBeEmpty();
+    expect($domain->title)->toBe('title');
+    expect($domain->description)->toBe('description');
+    expect($domain->yearLaunched)->toBe(2010);
+    expect($domain->duration)->toBe(50);
+    expect($domain->opened)->toBeTrue();
+    expect($domain->rating->value)->toBe('L');
     expect($domain->publish)->toBeFalse();
+    expect($domain->createdAt())->not->toBeEmpty();
+});
+
+test('add categories at video', function () {
+    $domain = new Video(
+        title: 'title',
+        description: 'description',
+        yearLaunched: 2010,
+        duration: 50,
+        opened: true,
+        rating: Rating::L,
+    );
+
+    $domain->addCategory('123');
+    $domain->addCategory('456');
+
+    expect($domain->categories)->toHaveCount(2);
+});
+
+test('remove categories at video', function () {
+    $domain = new Video(
+        title: 'title',
+        description: 'description',
+        yearLaunched: 2010,
+        duration: 50,
+        opened: true,
+        rating: Rating::L,
+        categories: ['456', '789']
+    );
+
+    $domain->removeCategory('123');
+    expect($domain->categories)->toHaveCount(2);
+
+    $domain->removeCategory('456');
+    expect($domain->categories)->toHaveCount(1);
+});
+
+test('add genres at video', function () {
+    $domain = new Video(
+        title: 'title',
+        description: 'description',
+        yearLaunched: 2010,
+        duration: 50,
+        opened: true,
+        rating: Rating::L,
+    );
+
+    $domain->addGenre('123');
+    $domain->addGenre('456');
+
+    expect($domain->genres)->toHaveCount(2);
+});
+
+test('remove genres at video', function () {
+    $domain = new Video(
+        title: 'title',
+        description: 'description',
+        yearLaunched: 2010,
+        duration: 50,
+        opened: true,
+        rating: Rating::L,
+        genres: ['456', '789']
+    );
+
+    $domain->removeGenre('123');
+    expect($domain->genres)->toHaveCount(2);
+
+    $domain->removeGenre('456');
+    expect($domain->genres)->toHaveCount(1);
+});
+
+test('add cast member at video', function () {
+    $domain = new Video(
+        title: 'title',
+        description: 'description',
+        yearLaunched: 2010,
+        duration: 50,
+        opened: true,
+        rating: Rating::L,
+    );
+
+    $domain->addCastMember('123');
+    $domain->addCastMember('456');
+
+    expect($domain->castMembers)->toHaveCount(2);
+});
+
+test('remove cast member at video', function () {
+    $domain = new Video(
+        title: 'title',
+        description: 'description',
+        yearLaunched: 2010,
+        duration: 50,
+        opened: true,
+        rating: Rating::L,
+        castMembers: ['456', '789']
+    );
+
+    $domain->removeCastMember('123');
+    expect($domain->castMembers)->toHaveCount(2);
+
+    $domain->removeCastMember('456');
+    expect($domain->castMembers)->toHaveCount(1);
 });
