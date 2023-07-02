06bfee826adc9bd52670ab4f9395f5352d1e90fd
Bruno Costa <bruno.costa@dafiti.com.br>
2023-07-01 22:10:29 -0300

    finalizado

--- src/Domain/Builder/Video/UpdateBuilderVideo.php
+++ src/Domain/Builder/Video/UpdateBuilderVideo.php
@@ -2,11 +2,7 @@
 
 namespace BRCas\MV\Domain\Builder\Video;
 
-use BRCas\CA\Domain\Abstracts\EntityAbstract;
-use BRCas\CA\Domain\ValueObject\Uuid;
 use BRCas\MV\Domain\Entity\Video;
-use BRCas\MV\Domain\Enum\Rating;
-use DateTime;
 
 class UpdateBuilderVideo extends CreateBuilderVideo
 {
